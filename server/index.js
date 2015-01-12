f1247999ca6b7c7153e398899a45e5b1fee35988
Aftab Alam <aftabbuddy@gmail.com>
2015-01-12 14:34:45 +0530

    Add starter code

--- server/index.js
+++ server/index.js
@@ -7,6 +7,8 @@ var   restify 	= require('restify')
 	////////////////
 	, server_port = 5001
 
+	, allow_CORS  = true
+
 
 
 	///////////////
@@ -14,8 +16,30 @@ var   restify 	= require('restify')
 	, server  	= restify.createServer();
 
 
+	server.use(restify.acceptParser(server.acceptable));
+	server.use(restify.queryParser());
+	server.use(restify.bodyParser());
+	server.use(morgan('dev')); 
+ 
+	// CORS
+	if(allow_CORS){
+		server.use(function(req, res, next) {
+		    res.header('Access-Control-Allow-Origin', "*");
+		    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+		    res.header('Access-Control-Allow-Headers', 'Content-Type');
+		    next();
+		});
+	}
+
+
+	server.get('/', function(req, res, next){
+		res.json(200,{'data':'key'});
+		return next();
+	});
+ 
+
 
 // Start Server...
-server.listen(server_port, function(){
-	console.log(server_port);
-});
+	server.listen(process.env.PORT || server_port, function(){
+		console.log("Server started @ ",process.env.PORT || server_port);
+	});
